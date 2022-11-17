9127c5479424ba3e87a3cb75eee6f3a76b049f21
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-17 21:39:03 +0530

    ADDED: Code to check Employee monthly wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,32 +1,32 @@
 import java.util.Random;
+
 public class EmployeeWageBuilder {
     int IS_ABSENT = 0;
     int IS_FULL_TIME = 1;
     int IS_PART_TIME = 2;
     int EMP_RATE_PER_HOUR = 20;
-    int FULL_DAY_HOUR = 8;
-    int PART_TIME_HOUR = 4;
-
+    int FULL_MONTH = 20;
     public static void main(String[] args) {
         EmployeeWageBuilder emp = new EmployeeWageBuilder();
         emp.attendanceCheck();
     }
-    public void attendanceCheck() {
+    public void attendanceCheck(){
         int empHrs = 0;
-        int daily_wage = 0;
-        Random random = new Random();
-        int empCheck = random.nextInt() % 3;
-        switch (empCheck) {
-            case 1:
-                empHrs = 8;
-                break;
-            case 2:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
+        int daily_Wage = 0;
+        int monthly_Wage = 0;
+        Random random=new Random();
+        int empCheck=random.nextInt()%3;
+
+        if (empCheck==IS_FULL_TIME){
+            empHrs = 8;
+        } else if (empCheck==IS_PART_TIME) {
+            empHrs = 4;
+
+        } else if (empCheck==IS_ABSENT){
+            empHrs = 0;
         }
-        daily_wage = (empHrs * EMP_RATE_PER_HOUR);
-        System.out.println("Employee Daily wage is :" + daily_wage);
+        daily_Wage = (empHrs*EMP_RATE_PER_HOUR);
+        monthly_Wage = (FULL_MONTH*empHrs*EMP_RATE_PER_HOUR);
+        System.out.println("Employee Monthly wage is :"+ monthly_Wage+" & daily wage is :"+daily_Wage);
     }
 }
