be602fab55a0755248c2b13d02cb93e266be6230
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-18 16:55:06 +0530

    MODIFIED: Code to check total wage till condition reached

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,41 +1,39 @@
 import java.util.Random;
-
 public class EmployeeWageBuilder {
-    int IS_ABSENT = 0;
-    int IS_FULL_TIME = 1;
-    int IS_PART_TIME = 2;
-    int EMP_RATE_PER_HOUR = 20;
-    int FULL_MONTH = 20;
-    int MAXIMUM_WORKING_HOUR = 100;
+    //CONSTANTS
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int FULL_MONTH_WORKING_DAYS = 20;
+    public static final int MAXIMUM_WORKING_HOUR = 100;
 
     public static void main(String[] args) {
         EmployeeWageBuilder emp = new EmployeeWageBuilder();
         emp.WageCheck();
     }
-
     public void WageCheck() {
         int empHrs = 0;
-        int daily_Wage = 0;
-        int monthly_Wage = 0;
         int totalEmpHrs = 0;
         int totalSalary = 0;
         int totalWorkingDays = 0;
         Random random = new Random();
-        int empCheck = random.nextInt() % 3;
-        while (totalEmpHrs < MAXIMUM_WORKING_HOUR && totalWorkingDays < FULL_MONTH) {
-            if (empCheck == IS_FULL_TIME) {
-                empHrs = 8;
-            } else if (empCheck == IS_PART_TIME) {
-                empHrs = 4;
-
-            } else if (empCheck == IS_ABSENT) {
-                empHrs = 0;
+        while (totalEmpHrs <= MAXIMUM_WORKING_HOUR && totalWorkingDays < FULL_MONTH_WORKING_DAYS) {
+            totalWorkingDays = totalWorkingDays + 1;
+            int empCheck = random.nextInt() % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
             }
-            totalEmpHrs = totalEmpHrs + empHrs;
-            totalWorkingDays++;
+            totalEmpHrs = empHrs + totalEmpHrs;
+            System.out.println("Day: " + totalWorkingDays + " Employee Hour: " + empHrs);
         }
-        totalSalary = totalEmpHrs * empHrs;
-        System.out.println(totalSalary);
-
+        totalSalary = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Total Employee Wage: " + totalSalary);
     }
 }
