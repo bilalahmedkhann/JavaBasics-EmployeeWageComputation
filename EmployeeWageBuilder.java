e9944e1c8e56060e03b7674098c1cdc13e48ffd8
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-19 00:15:40 +0530

    MODIFIED: Code to calculate employee wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,23 +1,17 @@
-import java.util.Random;
-
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
+        //Constants
         int EMP_RATE_PER_HOUR = 20;
-        int FULL_DAY_HOUR = 8;
-        EmployeeWageBuilder emp = new EmployeeWageBuilder();
-        emp.attendanceCheck(IS_FULL_TIME,EMP_RATE_PER_HOUR,FULL_DAY_HOUR);
-
-    }
-    public void attendanceCheck(int IS_FULL_TIME,int EMP_RATE_PER_HOUR,int FULL_DAY_HOUR){
-        Random random=new Random();
-        int empCheck=random.nextInt()%2;
-
-        if (empCheck==IS_FULL_TIME){
-            int total_Salary=(FULL_DAY_HOUR*EMP_RATE_PER_HOUR);
-            System.out.println("Employee is present & his daily wage is :"+total_Salary);
-        }else {
-            System.out.println("Employee is absent & his daily wage is :0");
-        }
+        int IS_FULL_TIME = 1;
+        //Variables
+        int empHrs = 0;
+        int empWage = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if(empCheck == IS_FULL_TIME)
+            empHrs = 8;
+        else
+            empHrs = 0;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage Per Day = " + empWage);
     }
 }
