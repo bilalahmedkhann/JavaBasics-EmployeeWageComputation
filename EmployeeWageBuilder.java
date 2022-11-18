70ca47d21fe0a6cd2301b716cc5e953f155bbb35
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-18 23:58:05 +0530

    MODIFIED: Code to add part time employee wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,33 +1,24 @@
-import java.util.Random;
-
 public class EmployeeWageBuilder {
+    //Constant declaration
+    int IS_FULL_TIME = 1;
+    int IS_PART_TIME = 2;
+    int EMP_RATE_PER_HOUR = 20;
+
     public static void main(String[] args) {
-        int IS_ABSENT = 0;
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
-        int FULL_DAY_HOUR = 8;
         EmployeeWageBuilder emp = new EmployeeWageBuilder();
-        emp.attendanceCheck(IS_FULL_TIME,EMP_RATE_PER_HOUR,FULL_DAY_HOUR,IS_PART_TIME,IS_ABSENT);
-
+        emp.wageCheck();
     }
-    public void attendanceCheck(int IS_FULL_TIME,int EMP_RATE_PER_HOUR,int FULL_DAY_HOUR, int IS_PART_TIME, int IS_ABSENT){
+    public void wageCheck() {
         int empHrs = 0;
-        int dailyWage = 0;
-        Random random=new Random();
-        int empCheck=random.nextInt()%3;
-
-        if (empCheck==IS_FULL_TIME){
+        int empWage = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_FULL_TIME) {
             empHrs = 8;
-            //int total_Salary=(FULL_DAY_HOUR*EMP_RATE_PER_HOUR);
-            //System.out.println("Employee is present & his daily wage is :"+total_Salary);
-        } else if (empCheck==IS_PART_TIME) {
+        } else if (empCheck == IS_PART_TIME) {
             empHrs = 4;
-
-        } else if (empCheck==IS_ABSENT){
+        } else
             empHrs = 0;
-        }
-        int daily_wage = (empHrs*EMP_RATE_PER_HOUR);
-        System.out.println("Employee Daily wage is :"+ daily_wage);
+        empWage = (empHrs * EMP_RATE_PER_HOUR);
+        System.out.println("Employee Daily wage is :" + empWage);
     }
 }
