651a08c05446a6933378b4af42bd4f49627f1cac
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-19 00:17:00 +0530

    MODIFIED: Code to calculate employee wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,24 +1,17 @@
 public class EmployeeWageBuilder {
-    //Constant declaration
-    int IS_FULL_TIME = 1;
-    int IS_PART_TIME = 2;
-    int EMP_RATE_PER_HOUR = 20;
-
     public static void main(String[] args) {
-        EmployeeWageBuilder emp = new EmployeeWageBuilder();
-        emp.wageCheck();
-    }
-    public void wageCheck() {
+        //Constants
+        int EMP_RATE_PER_HOUR = 20;
+        int IS_FULL_TIME = 1;
+        //Variables
         int empHrs = 0;
         int empWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME) {
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if(empCheck == IS_FULL_TIME)
             empHrs = 8;
-        } else if (empCheck == IS_PART_TIME) {
-            empHrs = 4;
-        } else
+        else
             empHrs = 0;
-        empWage = (empHrs * EMP_RATE_PER_HOUR);
-        System.out.println("Employee Daily wage is :" + empWage);
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage Per Day = " + empWage);
     }
 }
