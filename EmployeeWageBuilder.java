0d1f303af5995e0b1c38f63a754d34b19cec199a
Bilal Ahmed Khan <bilal.ahmed98@gmail.com>
2022-11-18 23:27:21 +0530

    MODIFIED: Code to check Employee wage using switch

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,32 +1,28 @@
-import java.util.Random;
 public class EmployeeWageBuilder {
-    int IS_ABSENT = 0;
-    int IS_FULL_TIME = 1;
-    int IS_PART_TIME = 2;
-    int EMP_RATE_PER_HOUR = 20;
-    int FULL_DAY_HOUR = 8;
-    int PART_TIME_HOUR = 4;
+    //Constant declaration
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
 
     public static void main(String[] args) {
         EmployeeWageBuilder emp = new EmployeeWageBuilder();
-        emp.attendanceCheck();
+        emp.wageCheck();
     }
-    public void attendanceCheck() {
+    public void wageCheck() {
         int empHrs = 0;
-        int daily_wage = 0;
-        Random random = new Random();
-        int empCheck = random.nextInt() % 3;
+        int empWage = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         switch (empCheck) {
-            case 1:
+            case IS_FULL_TIME:
                 empHrs = 8;
                 break;
-            case 2:
+            case IS_PART_TIME:
                 empHrs = 4;
                 break;
             default:
                 empHrs = 0;
         }
-        daily_wage = (empHrs * EMP_RATE_PER_HOUR);
-        System.out.println("Employee Daily wage is :" + daily_wage);
+        empWage = (empHrs * EMP_RATE_PER_HOUR);
+        System.out.println("Employee Daily wage is :" + empWage);
     }
 }
